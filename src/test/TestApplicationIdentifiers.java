e49207e5be500c19ed0bccdbb7d6f4d6f2e94dfe
Török Viktor <torokv@vnet.hu>
2020-01-02 18:50:01 +0100

    Modifying unit tests

--- src/test/TestApplicationIdentifiers.java
+++ src/test/TestApplicationIdentifiers.java
@@ -1,4 +1,5 @@
 package test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -14,53 +15,51 @@ class TestApplicationIdentifiers {
 	@Test
 	void ApplicationIdentifiersContainsAi01() {
 		ApplicationIdentifiers ids = new ApplicationIdentifiers();
-		
+
 		boolean contains = ids.contains("01");
-		
+
 		assertTrue(contains);
 	}
 
 	@Test
 	void ApplicationIdentifiersNotContainsAi800() {
 		ApplicationIdentifiers ids = new ApplicationIdentifiers();
-		
+
 		boolean contains = ids.contains("800");
-		
+
 		assertFalse(contains);
 	}
-	
+
 	@Test
-	void GetApplicationIdentifierAi01ReturnsNotNull()
-	{
+	void GetApplicationIdentifierAi01ReturnsNotNull() {
 		ApplicationIdentifiers ids = new ApplicationIdentifiers();
-		
+
 		ApplicationIdentifier id = ids.getIdentifier("01");
-		
+
 		assertNotNull(id);
 	}
-	
+
 	@Test
-	void GetApplicationIdentifierAi01ReturnsAi01()
-	{
+	void GetApplicationIdentifierAi01ReturnsAi01() {
 		ApplicationIdentifiers ids = new ApplicationIdentifiers();
-		
+
 		ApplicationIdentifier id = ids.getIdentifier("01");
-		
-		assertEquals("01", id.getAi());
-		assertEquals(AiDataLengthType.FIXED, id.getDataLengthType());
-		assertEquals(14, id.getMaxLength());
-		assertEquals(AiDataFormat.NUMERIC, id.getDataFormat());
-		assertEquals(AiDataType.NUMBER, id.getDataType());
-		assertEquals("GTIN", id.getDataTitle());
+
+		assertAll(
+				() -> assertEquals("01", id.getAi()),
+				() -> assertEquals(AiDataLengthType.FIXED, id.getDataLengthType()),
+				() -> assertEquals(14, id.getMaxLength()),
+				() -> assertEquals(AiDataFormat.NUMERIC, id.getDataFormat()),
+				() -> assertEquals(AiDataType.NUMBER, id.getDataType()),
+				() -> assertEquals("GTIN", id.getDataTitle()));
 	}
-	
+
 	@Test
-	void GetApplicationIdentifierAi800ReturnsNull()
-	{
+	void GetApplicationIdentifierAi800ReturnsNull() {
 		ApplicationIdentifiers ids = new ApplicationIdentifiers();
-		
+
 		ApplicationIdentifier id = ids.getIdentifier("800");
-		
+
 		assertNull(id);
 	}
 }
