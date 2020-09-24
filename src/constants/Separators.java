ac3a0efc41a7492d4a0e8a0426df7cb154e53c03
Viktor Török <torokv@vnet.hu>
2020-09-24 13:22:23 +0200

    Separators.java: some comments

---
+++ src/constants/Separators.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class Separators
+{
+	// FNC1 separator constants
+	public static final char SEPARATOR_SPACE = ' ';
+	public static final char SEPARATOR_ZEBRA = (char)29;
+}
